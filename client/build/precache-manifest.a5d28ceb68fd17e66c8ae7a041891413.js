0cefdc4bc164bb5a02cf3939d99bf68b426a92dc
manwlispro@gmail.com <manwlispro@gmail.com>
2020-06-21 20:11:42 +0300

    fdfd

---
+++ client/build/precache-manifest.a5d28ceb68fd17e66c8ae7a041891413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff8a41529deeca175e3d7d4f385d2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d05eccf9d12cddcefbd",
+    "url": "/static/css/main.e4753289.chunk.css"
+  },
+  {
+    "revision": "bfb42d1befbbd9c1841f",
+    "url": "/static/js/2.94441964.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.94441964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d05eccf9d12cddcefbd",
+    "url": "/static/js/main.42195784.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4854507e36ce9dce49fc2c3d513b1816",
+    "url": "/static/media/testLogo.4854507e.png"
+  }
+]);
